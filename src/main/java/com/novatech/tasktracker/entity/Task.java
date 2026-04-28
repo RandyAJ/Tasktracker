c89ae475efa3d69fd3fb71b43246702409a59569
Asota Randy <randy201312@gmail.com>
2026-04-29 02:17:08 +0300

    доработка сущности

--- src/main/java/com/novatech/tasktracker/entity/Task.java
+++ src/main/java/com/novatech/tasktracker/entity/Task.java
@@ -1,22 +1,49 @@
 package com.novatech.tasktracker.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.AssertTrue;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "task")
+@NoArgsConstructor
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
-    Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) // ID генерируется до Insert в БД, можно использовать пакетную вставку (batching)
+    private Long id;
 
-    String title;
-    String description;
+    @NotNull // на уровне кода до сэйва
+    private String title;
+
+    private String description;
+
     @Enumerated(EnumType.STRING)
-    TaskStatus status;
+    @Column(nullable = false) // на уровне БД потому что задаю пресейвом
+    private TaskStatus status;
 
-    LocalDateTime dateCreated;
-    LocalDateTime dateDeadline;
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateDeadline;
+
+    @PrePersist
+    protected void onCreate() {
+        if(this.dateCreated == null){
+            this.dateCreated = LocalDateTime.now();
+            System.out.println("Сущность будет сохранена с датой: " + this.dateCreated); // логировать в сервисе.
+        }
+
+        if(this.status == null){
+            this.status = TaskStatus.BACKLOG;
+            System.out.println("Сущность будет сохранена со статусом: " + this.status); // логировать в сервисе.
+        }
+    }
+
+    @AssertTrue
+    public boolean isDeadlineValid() {
+        return dateDeadline == null || !dateDeadline.isBefore(dateCreated); // дата создания должна быть раньше дедлайна. либо дедлайн может быть пустым
+    }
 }
